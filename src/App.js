bc538af778a0da607cb1d0d30dd85eaf88f9ece7
Vikas Kumar Saw <vikasvks0112@gmail.com>
2021-05-06 19:00:58 +0530

    added context provider for oxygen

--- src/App.js
+++ src/App.js
@@ -1,68 +1,20 @@
 import React from "react";
 import "./assets/styles/main.css";
+import Navigation from "./components/homePage/Navigation";
+import OxygenContextProvider from "./contexts/OxygenContextProvider";
 import HomePage from "./pages/HomePage";
 
 function App() {
+  /*OxygenContextProvider is a context provider that has 
+  been created in src/contexts/OxygenContextProvider.js 
+  it acts as a central data storage for the cards that display oxygen data
+  */
   return (
     <>
-      <HomePage />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      oidihyihsdfuh
+      <OxygenContextProvider>
+        <Navigation />
+        <HomePage />
+      </OxygenContextProvider>
     </>
   );
 }
