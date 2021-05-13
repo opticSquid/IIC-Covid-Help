6df9b3a54a4e81325c0cf5e3f02ebc0c8d3ed03d
Shadow129-sys <gsayankr02@gmail.com>
2021-05-13 15:01:44 +0530

    error page fixed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./assets/styles/main.css";
 import { useStateContext } from "./contexts/ContextProvider";
 import checkJWT from "./components/Checkjwt";
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
 } from "react-router-dom";
+//import loading from "./svgs/loading.gif";
 
 // Lazy loading the components on demand to load faster
 const HomePage = lazy(() => import("./pages/HomePage"));
@@ -19,49 +20,48 @@ const Hospitals = lazy(() => import("./components/hospitals/Hospitals"));
 const EditHospital = lazy(() => import("./pages/EditHospital"));
 const Error = lazy(() => import("./errorPage/error"));
 function App() {
-  const [{ origin }] = useStateContext();
-  useEffect(() => {
-    //If user is already logged in
-    if (localStorage.getItem("refreshToken")) {
-      // Regenerating new access token
-      checkJWT(origin)
-        .then((res) => {
-          if (res) {
-            console.log("New access token generated");
-          } else {
-            console.log("Old Access token still valid");
-          }
-        })
-        .catch((error) => {
-          console.log("Check jwt could not be called", error);
-        });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return (
-    <Router>
-      <Suspense
-        fallback={
-          //Put some loading animation here later
-          <div>
-            <h1>Loading...</h1>
-          </div>
+    const [{ origin }] = useStateContext();
+    useEffect(() => {
+        //If user is already logged in
+        if (localStorage.getItem("refreshToken")) {
+            // Regenerating new access token
+            checkJWT(origin)
+                .then((res) => {
+                    if (res) {
+                        console.log("New access token generated");
+                    } else {
+                        console.log("Old Access token still valid");
+                    }
+                })
+                .catch((error) => {
+                    console.log("Check jwt could not be called", error);
+                });
         }
-      >
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/hospitals" component={Hospitals} />
-          <Route path="/verify" component={Verify} />
-          <Route path="/edit/:uid" component={EditHospital} />
-          <Route path="/error/:id" children={<Error />} />
-          <Redirect to="/" />
-        </Switch>
-      </Suspense>
-    </Router>
-  );
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    return (
+        <Router>
+            <Suspense
+                fallback={
+                    //Put some loading animation here later
+                    <div>
+                        <h2>Loading...</h2>
+                    </div>
+                }>
+                <Switch>
+                    <Route exact path='/' component={HomePage} />
+                    <Route path='/about' component={AboutPage} />
+                    <Route path='/login' component={Login} />
+                    <Route path='/signup' component={Signup} />
+                    <Route path='/hospitals' component={Hospitals} />
+                    <Route path='/verify' component={Verify} />
+                    <Route path='/edit/:uid' component={EditHospital} />
+                    <Route path='/error/:id' children={<Error />} />
+                    <Redirect to='/' />
+                </Switch>
+            </Suspense>
+        </Router>
+    );
 }
 
 export default App;
--- src/signInUpPages/SignIn.js
+++ src/signInUpPages/SignIn.js
@@ -6,107 +6,116 @@ import Axios from "axios";
 import "./signIn.css";
 
 const Signin = () => {
-  const [{ origin }] = useStateContext();
-  const history = useHistory();
-  return (
-    <>
-      <div className="signIn">
-        <Logo />
-        <Form origin={origin} history={history} />
-      </div>
-    </>
-  );
+    const [{ origin }] = useStateContext();
+    const history = useHistory();
+    return (
+        <>
+            <div className='signIn'>
+                <Logo />
+                <Form origin={origin} history={history} />
+            </div>
+        </>
+    );
 };
 
 const Logo = () => {
-  return (
-    <>
-      <section className="logoSignIn">
-        <Link to="/">
-          <img
-            style={{ maxWidth: "30em", paddingBottom: "4em", cursor:"pointer"}}
-            src={logoImg}
-            alt="Logo"
-          />
-        </Link>
-        <h2>Helping people connect to the emergency services</h2>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section className='logoSignIn'>
+                <Link to='/'>
+                    <img
+                        style={{
+                            maxWidth: "30em",
+                            paddingBottom: "4em",
+                            cursor: "pointer",
+                        }}
+                        src={logoImg}
+                        alt='Logo'
+                    />
+                </Link>
+                <h2>Helping people connect to the emergency services</h2>
+            </section>
+        </>
+    );
 };
 
 const Form = ({ origin, history }) => {
-  const [email, SetEmail] = useState("");
-  const [password, SetPassword] = useState("");
-  const submitHandler = (e) => {
-    e.preventDefault();
-    if (email === "") {
-      alert("Please enter your email");
-    } else {
-      if (password === "") {
-        alert("Please enter the password");
-      } else {
-        let newUser = { Email: email, Password: password };
-        Axios.post(`${origin}/login`, newUser)
-          .then((response) => {
-            console.log("login response", response.data.tokens.Name);
-            if (response.data.status === "Logged in successfully") {
-              localStorage.setItem(
-                "refreshToken",
-                response.data.tokens.refreshToken
-              );
-              sessionStorage.setItem(
-                "accessToken",
-                response.data.tokens.accessToken
-              );
-              localStorage.setItem("userName", response.data.tokens.Name);
-              history.push("/");
+    const [email, SetEmail] = useState("");
+    const [password, SetPassword] = useState("");
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if (email === "") {
+            alert("Please enter your email");
+        } else {
+            if (password === "") {
+                alert("Please enter the password");
             } else {
-              //signin error
-              history.push("error/1");
+                let newUser = { Email: email, Password: password };
+                Axios.post(`${origin}/login`, newUser)
+                    .then((response) => {
+                        //console.log("login response", response.data.tokens.Name);
+                        if (response.data.status === "Logged in successfully") {
+                            localStorage.setItem(
+                                "refreshToken",
+                                response.data.tokens.refreshToken
+                            );
+                            sessionStorage.setItem(
+                                "accessToken",
+                                response.data.tokens.accessToken
+                            );
+                            localStorage.setItem(
+                                "userName",
+                                response.data.tokens.Name
+                            );
+                            history.push("/");
+                        } else {
+                            //signin error
+                            history.push("error/1");
+                        }
+                    })
+                    .catch((error) => {
+                        if (error) {
+                            console.log(
+                                "Error occoured while signing up",
+                                error
+                            );
+                        }
+                    });
             }
-          })
-          .catch((error) => {
-            if (error) {
-              console.log("Error occoured while signing up", error);
-            }
-          });
-      }
-    }
-  };
+        }
+    };
 
-  return (
-    <form className="form">
-      <input
-        type="email"
-        onChange={(e) => SetEmail(e.target.value)}
-        className="input"
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        onChange={(e) => SetPassword(e.target.value)}
-        className="input"
-        placeholder="Password"
-        required
-      />
-      <button
-        type="submit"
-        onClick={(e) => submitHandler(e)}
-        className="signin"
-      >
-        Sign In
-      </button>
-      <button className="forgetPassword">Forget Password?</button>
-      <p>
-        Don't Have an Account?{" "}
-        <Link to="/signup" className="signup">
-          Sign Up
-        </Link>
-      </p>
-    </form>
-  );
+    return (
+        <form className='form'>
+            <input
+                type='email'
+                onChange={(e) => SetEmail(e.target.value)}
+                className='input'
+                placeholder='Email'
+                required
+            />
+            <input
+                type='password'
+                onChange={(e) => SetPassword(e.target.value)}
+                className='input'
+                placeholder='Password'
+                required
+            />
+            <button
+                type='submit'
+                onClick={(e) => submitHandler(e)}
+                className='signin'>
+                Sign In
+            </button>
+            <button className='forgetPassword'>Forget Password?</button>
+            <p>
+                Don't Have an Account?{" "}
+                <Link to='/signup' className='signup'>
+                    Sign Up
+                </Link>
+            </p>
+        </form>
+    );
 };
 
 export default Signin;
