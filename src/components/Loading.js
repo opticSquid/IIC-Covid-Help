1f7238fe1b21869ba94fff12047aaac59ae55816
codeswithroh <codeswithroh@users.noreply.github.com>
2021-05-14 11:38:09 +0530

    Loading componet

---
+++ src/components/Loading.js
@@ -0,0 +1,20 @@
+import {WaveTopBottomLoading} from 'react-loadingg';
+import React, { Component } from 'react';
+
+class Loading extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div style={{height:"100vh", display:"grid", placeItems:"center"}}>
+                <div>
+                    <WaveTopBottomLoading size="large" />
+                </div>
+                <div>
+                    <h1>Please wait....</h1>
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default Loading;
