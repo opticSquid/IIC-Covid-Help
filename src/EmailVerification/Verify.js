d573bdc7de9ef0409586bb961437d5b97ca4f391
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2021-05-23 21:00:28 +0530

    Adding error hadling in email verification

--- src/EmailVerification/Verify.js
+++ src/EmailVerification/Verify.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import IsOk from "../svgs/ok.svg";
 import "./style.css";
 
 const Verifyemail = () => {
+  const { vmsg } = useParams();
   return (
     <div className="verifiedBack">
       <div className="verifieddiv">
         <h3 className="Veri">
-          Verification mail sent also please remember to check your spam folder
+          {vmsg}
           <img src={IsOk} alt="ok text" className="okimg"></img>
         </h3>
       </div>
