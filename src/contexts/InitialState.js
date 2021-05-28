11d61b073411f8389d16a64999982509a2c6d677
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2021-05-28 16:21:57 +0530

    changed backend origin

--- src/contexts/InitialState.js
+++ src/contexts/InitialState.js
@@ -2,7 +2,7 @@ export const InitialState = {
   //origin: "https://vast-scrubland-97353.herokuapp.com",
   //origin: "http://localhost:5000",
   origin:
-    "http://iicmybondhuserver-env.eba-fmsmrizf.ap-south-1.elasticbeanstalk.com",
+    "http://iicmybondhuserver-env-1.eba-kahpemnk.ap-south-1.elasticbeanstalk.com",
   accessToken: undefined,
   Oxygen: "",
   Normal: "",
